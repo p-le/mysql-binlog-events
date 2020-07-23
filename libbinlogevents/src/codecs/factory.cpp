604290306fc14d91da8bded04868ffdc4f70ad79
Phu Le <phu.le.dev@gmail.com>
2020-07-23 09:17:42 +0900

    update libbinlogevents

--- libbinlogevents/src/codecs/factory.cpp
+++ libbinlogevents/src/codecs/factory.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -20,19 +20,23 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
-#ifndef BINARY_LOG_INCLUDED
-#define BINARY_LOG_INCLUDED
+#include <codecs/binary.h>
+#include <codecs/factory.h>
 
-/*
-  This file is a convenience header file meant for the users, for inclusion
-  of other related header files. Please refrain from adding any definitions
-  to this header.
-*/
-#include "binlog_event.h"
-#include "control_events.h"
-#include "field_types.h"
-#include "load_data_events.h"
-#include "rows_event.h"
-#include "statement_events.h"
+namespace binary_log {
+namespace codecs {
 
-#endif /* BINARY_LOG_INCLUDED */
+std::unique_ptr<Codec> Factory::build_codec(Log_event_type t) {
+  switch (t) {
+    case TRANSACTION_PAYLOAD_EVENT:
+      return std::make_unique<
+          binary_log::codecs::binary::Transaction_payload>();
+    default:              /* purecov: inspected */
+      BAPI_ASSERT(false); /* purecov: inspected */
+  }
+
+  return nullptr;
+}
+
+}  // namespace codecs
+}  // namespace binary_log
